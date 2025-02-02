9fe316d67c1a28066d8c2a73c09db59161d97632
soloscribblecreatives <34957574+soloscribblecreatives@users.noreply.github.com>
2025-02-03 04:57:36 +0530

    update

--- atoderma3/getUserAction.js
+++
@@ -1,9 +0,0 @@
-const queryString = window.location.search;
-
-const urlParams = new URLSearchParams(queryString);
-
-const page_type = urlParams.get('jsondata')
-
-
-
-document.getElementById('clickEventDuration').innerHTML = JSON.parse(page_type);
